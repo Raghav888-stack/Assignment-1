c3dbfc60ac39e629d28e926b19c048344eadf748
Raghav888-stack <raghavachess99@gmail.com>
2025-09-10 16:52:45 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+
+a = float(input('enter the first number :  '))
+b = float(input('enter the second number:  '))
+sum = a + b
+sub = a - b
+mul = a*b
+div = a/b
+print("Addition ",sum)
+print("Substraction  ", sub)
+print("Multiplication ",mul)
+print("Division ",div)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+str1 = input("Enter your first name: ")
+str2 = input("Enter ypur last name: ")
+str = str1 + str2
+
+print("Hello,",str ,"! Welcome to python program." )
